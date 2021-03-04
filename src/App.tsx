077ece401777689f26397f0bec1a2b2b047f2c0c
thedon122 <donovan122@yahoo.com>
2021-03-03 23:56:54 -0500

    set up main app file

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+
+function App() {
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch('http://localhost:8000/api/user', {
+                    headers: {'Content-Type': 'application/json'},
+                    credentials: 'include',
+                });
+
+                const content = await response.json();
+
+                setName(content.name);
+            }
+        )();
+    });
