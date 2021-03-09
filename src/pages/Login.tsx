6ebb113e78bb7d16c9ebb8c544ff574162b95fd5
thedon122 <donovan122@yahoo.com>
2021-03-08 23:52:08 -0500

    api call from login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,3 +6,16 @@ const Login = (props: { setName: (name: string) => void }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [redirect, setRedirect] = useState(false);
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        const response = await fetch('http://localhost:8000/api/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include',
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
