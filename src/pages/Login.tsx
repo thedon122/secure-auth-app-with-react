a4a7f2674180e335634d900d751b5609302c4fa8
thedon122 <donovan122@yahoo.com>
2021-03-09 23:11:49 -0500

    added input and buttons for login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -28,3 +28,22 @@ const Login = (props: { setName: (name: string) => void }) => {
 
     if (redirect) {
         return <Redirect to="/"/>;
+    }
+
+    return (
+        <form onSubmit={submit}>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+            <input type="email" className="form-control" placeholder="Email address" required
+                   onChange={e => setEmail(e.target.value)}
+            />
+
+            <input type="password" className="form-control" placeholder="Password" required
+                   onChange={e => setPassword(e.target.value)}
+            />
+
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        </form>
+    );
+};
+
+export default Login;
