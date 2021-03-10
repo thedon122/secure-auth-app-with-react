b7c13f562752b518ff263d29cd3b66a76b1277a9
thedon122 <donovan122@yahoo.com>
2021-03-09 23:52:20 -0500

    setup register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,9 @@
+
+import React, {SyntheticEvent, useState} from 'react';
+import {Redirect} from 'react-router-dom';
+
+const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
