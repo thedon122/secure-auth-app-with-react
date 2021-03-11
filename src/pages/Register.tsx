bcca5a3b066f7ce485fed52f9bff7fada59cb908
thedon122 <donovan122@yahoo.com>
2021-03-10 23:56:50 -0500

    async post

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -7,3 +7,19 @@ const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [redirect, setRedirect] = useState(false);
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        await fetch('http://localhost:8000/api/register', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        });
+
+        setRedirect(true);
+    }
