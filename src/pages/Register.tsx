a86b30a453fe3fd0be93dd916a0ed28f0e25ec8a
thedon122 <donovan122@yahoo.com>
2021-03-10 23:57:42 -0500

    user input and button

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -23,3 +23,30 @@ const Register = () => {
 
         setRedirect(true);
     }
+
+    if (redirect) {
+        return <Redirect to="/login"/>;
+    }
+
+    return (
+        <form onSubmit={submit}>
+            <h1 className="h3 mb-3 fw-normal">Please register</h1>
+
+            <input className="form-control" placeholder="Name" required
+                   onChange={e => setName(e.target.value)}
+            />
+
+            <input type="email" className="form-control" placeholder="Email address" required
+                   onChange={e => setEmail(e.target.value)}
+            />
+
+            <input type="password" className="form-control" placeholder="Password" required
+                   onChange={e => setPassword(e.target.value)}
+            />
+
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default Register;
