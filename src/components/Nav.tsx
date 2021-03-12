e316cbb283c47b5991e4308e32f9fa1707d35e98
thedon122 <donovan122@yahoo.com>
2021-03-11 23:52:33 -0500

    api post

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,2 +1,15 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+
+const Nav = (props: { name: string, setName: (name: string) => void }) => {
+    const logout = async () => {
+        await fetch('http://localhost:8000/api/logout', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include',
+        });
+
+        props.setName('');
+    }
+
+    let menu;
