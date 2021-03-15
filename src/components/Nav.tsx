66b8a0d6e54bbf898c6feb33b7745c16ee14b856
thedon122 <donovan122@yahoo.com>
2021-03-14 23:45:59 -0400

    nav if statment

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,3 +13,15 @@ const Nav = (props: { name: string, setName: (name: string) => void }) => {
     }
 
     let menu;
+
+    if (props.name === '') {
+        menu = (
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <li className="nav-item active">
+                    <Link to="/login" className="nav-link">Login</Link>
+                </li>
+                <li className="nav-item active">
+                    <Link to="/register" className="nav-link">Register</Link>
+                </li>
+            </ul>
+        )
